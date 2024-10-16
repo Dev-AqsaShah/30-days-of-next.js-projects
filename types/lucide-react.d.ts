bbd531481910edf68cc7312ce60668b2fe202738
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-10-16 09:08:53 +0500

    Delete types directory

--- types/lucide-react.d.ts
+++
@@ -1,9 +0,0 @@
-import { Cloud, MapPin, Thermometer } from 'lucide-react';
-
-declare module 'lucide-react' {
-    // Add any specific type declarations for the module if you know them.
-    // For now, we declare the module as any type.
-    const content: any;
-    export default content;
-  }
-  
