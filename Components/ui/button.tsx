0d3fff57ab22175a1ef7eee8a7a1497a9ec5abff
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-10-16 09:08:15 +0500

    Delete Components directory

--- Components/ui/button.tsx
+++
@@ -1,9 +0,0 @@
-interface ButtonProps {
-    label: string;
-    onClick: () => void;
-  }
-  
-  export const Button: React.FC<ButtonProps> = (props) => {
-    return <button onClick={props.onClick}>{props.label}</button>;
-  };
-  
