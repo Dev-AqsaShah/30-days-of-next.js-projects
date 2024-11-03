9c21ea5ada60999a97075ab6b4133162bc7cd601
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-11-03 08:01:19 -0800

    Add files via upload

---
+++ day-5-calculator/app/page.tsx
@@ -0,0 +1,12 @@
+import Calculator from '@/Components/calculator'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  )
+}
+
+export default page
